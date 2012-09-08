e86aac7d1b06ee020b8394a90548889ced93792c
Murat GUZEL <guzelmurat@gmail.com>
2012-09-08 13:24:48 +0300

    cars list in index page

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ActiveRecord::Base
   attr_accessible :brand, :model, :title
   letsrate_rateable "performance", "price", "beauty"
+
+  def to_s
+  	[brand, model].compact.join(', ')
+  end
 end
